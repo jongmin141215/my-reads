c5f441badfc51e4816faa4f33822ccfc8ad1e05c
Jongmin Kim <kjm141215@gmail.com>
2017-12-08 21:53:02 -0800

    Get books from server and display them by category

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
+import * as BooksAPI from './BooksAPI';
 import BookShelf from './BookShelf';
 
 class App extends Component {
+  componentDidMount() {
+    const books = BooksAPI.getAll().then(books =>(
+      this.setState({ books })
+    ));
+  }
+  state = {
+    books: []
+  }
   render() {
+    console.log("current", this.state.books.filter(book => book.shelf === "currentlyReading"))
     return (
       <div>
-        <BookShelf category="Currently Reading"/>
-        <BookShelf category="Want to Read"/>
-        <BookShelf category="Read"/>
+        <BookShelf category="Currently Reading" books={this.state.books.filter(book => book.shelf === "currentlyReading")} />
+        <BookShelf category="Want to Read"  books={this.state.books.filter(book => book.shelf === "wantToRead")}/>
+        <BookShelf category="Read"  books={this.state.books.filter(book => book.shelf === "read")}/>
       </div>
     );
   }
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
 
 class BookShelf extends Component {
+  renderBookList(books) {
+    if (!books) {
+      return <h3>Currently no books in this shelf</h3>
+    }
+    return (
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>{book.title}</li>
+        ))}
+      </ul>
+    )
+  }
   render() {
+    console.log("books", this.props)
     return (
       <div>
         <h2>{this.props.category}</h2>
+        {this.renderBookList(this.props.books)}
       </div>
     );
   }
